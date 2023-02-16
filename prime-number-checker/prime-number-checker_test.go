bf2fff154ddf022480a1851c6cccec9e438be600
c0de-runn3r <109829568+c0de-runn3r@users.noreply.github.com>
2023-02-16 17:37:26 +0100

    prime-number-checker test coverage

--- prime-number-checker/prime-number-checker_test.go
+++ prime-number-checker/prime-number-checker_test.go
@@ -13,8 +13,33 @@ func assert(t *testing.T, a, b any) {
 
 func TestCheckNumbers(t *testing.T) {
 	assert(t, CheckNumbers([]int{1, 2, 3, 4, 5}), []bool{false, true, true, false, true})
+	assert(t, CheckNumbers([]int{11, 13, 17, 19, 23}), []bool{true, true, true, true, true})
+	assert(t, CheckNumbers([]int{100, 1000, 10000, 100000, 1000000}), []bool{false, false, false, false, false})
+	assert(t, CheckNumbers([]int{0, -1, -2, -3, -4, -5}), []bool{false, false, false, false, false, false})
 }
 
+func TestIsPrime(t *testing.T) {
+	assert(t, isPrime(-10), false)
+	assert(t, isPrime(0), false)
+	assert(t, isPrime(1), false)
+	assert(t, isPrime(2), true)
+	assert(t, isPrime(3), true)
+	assert(t, isPrime(4), false)
+	assert(t, isPrime(5), true)
+}
+
+func TestIsPrimeStd(t *testing.T) {
+	assert(t, isPrime(-10), false)
+	assert(t, isPrimeStd(0), false)
+	assert(t, isPrimeStd(1), false)
+	assert(t, isPrimeStd(2), true)
+	assert(t, isPrimeStd(3), true)
+	assert(t, isPrimeStd(4), false)
+	assert(t, isPrimeStd(5), true)
+}
+
+// These benchmarks shows the efficienty of my own prime number checker
+// comparing to one from big package on small values
 func BenchmarkStandartPrimeSmallRandomNumbers(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		isPrimeStd(n)
@@ -26,6 +51,8 @@ func BenchmarkCustomPrimeSmallRandomNumbers(b *testing.B) {
 	}
 }
 
+// These benchmarks shows the efficienty of prime number checker from big package
+// comparing to custom one if we are processing big values
 func BenchmarkStandartPrimeBigNumber(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		isPrimeStd(2147483647)
